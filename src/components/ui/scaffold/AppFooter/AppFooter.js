6df1206f9b1486d4121f82de307e790e381dd49d
Demonslyr <Demonslyr@users.noreply.github.com>
2020-05-19 16:04:08 -0500

    Update AppFooter.js

--- src/components/ui/scaffold/AppFooter/AppFooter.js
+++ src/components/ui/scaffold/AppFooter/AppFooter.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import { List, Image } from 'semantic-ui-react';
 import api from '../../../../api/';
 import appPkg from '../../../../package.json';
-import build from '../../../../build.json';
+import build from '../../../../../build.json';
 import moment from 'moment';
 
 import './AppFooter.css';
